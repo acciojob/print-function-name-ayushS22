37dfa7c83ba1c22d521118e5ef743738ff7d8535
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-09-09 07:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function functionName() {
+  alert(arguments.callee.name);
+}
+
+// Example calls
+functionName();  // alerts "functionName"
+
+function AccioJob() {
+  alert(arguments.callee.name);
+}
+
+AccioJob();  // alerts "AccioJob"
